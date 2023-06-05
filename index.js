03c889f5c960eeffce9b36a0871a5d394eac2afb
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-06-05 14:40:56 +0300

    Add the script tag for javascript

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const paragraph=document.createElement('p');
 const container=document.querySelector('remove');
 paragraph.innerHTML='first-book';
 
-container.appendChild(paragraph);
+container.append(paragraph);
