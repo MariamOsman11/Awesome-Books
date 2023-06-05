85b79a0c3e83a94717db82ab2395a6660d9f57cc
MariamOsman11 <mariamkca11@gmail.com>
2023-06-05 21:58:58 +0300

    Add changes into files

--- js/index.js
+++ js/index.js
@@ -1,36 +1,58 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const mainContainer = document.createElement('div');
-//     mainContainer.classList.add('added-books');
-//     const paragraphOne= document.createElement('p')
-//     paragraphOne.innerHTML = "first book";
-//     const paragraphTwo = document.createElement('p');
-//     paragraphTwo.innerHTML = "Written by Robert";
-//     const underline = document.createElement('hr');
-//     const buttonRemove = document.createElement('button');
-//     buttonRemove.innerHTML='Remove';
-//     mainContainer.appendChild(paragraphOne);
-//     mainContainer.appendChild(paragraphTwo);
-//     mainContainer.appendChild(buttonRemove);
-//     mainContainer.appendChild(underline);
-//     document.getElementById('bookList').appendChild(mainContainer);
-// });
-
-const title=document.querySelector('#title');
-const author=document.querySelector('#author');
-const formBtn=document.querySelector('#btn-add');
-const bookList=document.querySelector('#bookList');
- let arrayOfBooks=JSON.parse(localStorage.getItem('arrayOfBooks')) ||[];
+const BooksContainer = document.querySelector('.bookList');
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
+const form = document.querySelector('form');
 
 
-//function for the add
-function addBooks(){
+let bookLibrary = [];
 
+const store = localStorage.getItem('bookLibrary');
+    if( store) {
+       bookLibrary = JSON.parse( store);
+    }
+
+function createLibrary () {
+    BooksContainer.innerHTML = '';
+   bookLibrary.forEach((book, index) => {
+    const bookContainer = document.createElement('div');
+        bookContainer.innerHTML = `
+        <p>${book.title}</p>
+       <p> ${book.author}</p>
+            <button onclick="removeBook(${index})">Remove</button>
+        <hr>
+    `;
+    BooksContainer.appendChild(bookContainer);
+    });
+}
+
+// Add New Book to The List
+function addBook(e) {
+    e.preventDefault();
+    const newBooks = {
+        title: bookTitle.value,
+        author: bookAuthor.value,
+    };
+
+   bookLibrary.push(newBooks);
+    localStorage.setItem('books', JSON.stringify(bookLibrary));
+
+    createLibrary();
+
+    // Reset input fields
+    bookTitle.value = '';
+    bookAuthor.value = '';
+}
+// Add New Book to The List
+
+function removeBook(index) {
+   bookLibrary =bookLibrary.filter((book, bookIndex) => bookIndex !== index);
+    localStorage.setItem('bookLibrary',  JSON.stringify(bookLibrary));
+
+    createLibrary();
 }
 
 
 
+form.addEventListener('submit', addBook);
 
-//display function
-
-
-//function for remove
+createLibrary();
