37c5711091e27832b410503f0b0a5d599c6c44d3
MariamOsman11 <mariamkca11@gmail.com>
2023-06-05 16:33:58 +0300

    Add adjustment on Js file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const mainContainer = document.createElement('div');
-    mainContainer.classList.add('added-books');
-    const paragraphOne= document.createElement('p')
-    paragraphOne.innerHTML = "first book";
-    const paragraphTwo = document.createElement('p');
-    paragraphTwo.innerHTML = "Written by Robert";
-    const underline = document.createElement('hr');
-    const buttonRemove = document.createElement('button');
-    buttonRemove.innerHTML='Remove';
-    mainContainer.appendChild(paragraphOne);
-    mainContainer.appendChild(paragraphTwo);
-    mainContainer.appendChild(buttonRemove);
-    mainContainer.appendChild(underline);
-    document.getElementById('bookList').appendChild(mainContainer);
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//     const mainContainer = document.createElement('div');
+//     mainContainer.classList.add('added-books');
+//     const paragraphOne= document.createElement('p')
+//     paragraphOne.innerHTML = "first book";
+//     const paragraphTwo = document.createElement('p');
+//     paragraphTwo.innerHTML = "Written by Robert";
+//     const underline = document.createElement('hr');
+//     const buttonRemove = document.createElement('button');
+//     buttonRemove.innerHTML='Remove';
+//     mainContainer.appendChild(paragraphOne);
+//     mainContainer.appendChild(paragraphTwo);
+//     mainContainer.appendChild(buttonRemove);
+//     mainContainer.appendChild(underline);
+//     document.getElementById('bookList').appendChild(mainContainer);
+// });
+
+const title=document.querySelector('#title');
+const author=document.querySelector('#author');
+const formBtn=document.querySelector('#btn-add');
+const bookList=document.querySelector('#bookList');
+ let arrayOfBooks=JSON.parse(localStorage.getItem('arrayOfBooks')) ||[];
+
+
+//function for the add
+function addBooks(){
+
+}
+
+
+
+
+//display function
+
+
+//function for remove
