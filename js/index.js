45a15412bd22660b045250625dd176e3fac3a023
MariamOsman11 <mariamkca11@gmail.com>
2023-06-05 22:07:53 +0300

    Fix linters

--- js/index.js
+++ js/index.js
@@ -3,56 +3,54 @@ const bookTitle = document.querySelector('.title');
 const bookAuthor = document.querySelector('.author');
 const form = document.querySelector('form');
 
-
 let bookLibrary = [];
 
 const store = localStorage.getItem('bookLibrary');
-    if( store) {
-       bookLibrary = JSON.parse( store);
-    }
+if (store) {
+  bookLibrary = JSON.parse(store);
+}
 
-function createLibrary () {
-    BooksContainer.innerHTML = '';
-   bookLibrary.forEach((book, index) => {
+function createLibrary() {
+  BooksContainer.innerHTML = '';
+  bookLibrary.forEach((book, index) => {
     const bookContainer = document.createElement('div');
-        bookContainer.innerHTML = `
+    bookContainer.innerHTML = `
         <p>${book.title}</p>
        <p> ${book.author}</p>
             <button onclick="removeBook(${index})">Remove</button>
         <hr>
     `;
     BooksContainer.appendChild(bookContainer);
-    });
+  });
 }
 
 // Add New Book to The List
 function addBook(e) {
-    e.preventDefault();
-    const newBooks = {
-        title: bookTitle.value,
-        author: bookAuthor.value,
-    };
+  e.preventDefault();
+  const newBooks = {
+    title: bookTitle.value,
+    author: bookAuthor.value,
+  };
 
-   bookLibrary.push(newBooks);
-    localStorage.setItem('books', JSON.stringify(bookLibrary));
+  bookLibrary.push(newBooks);
+  localStorage.setItem('books', JSON.stringify(bookLibrary));
 
-    createLibrary();
+  createLibrary();
 
-    // Reset input fields
-    bookTitle.value = '';
-    bookAuthor.value = '';
+  // Reset input fields
+  bookTitle.value = '';
+  bookAuthor.value = '';
 }
+
 // Add New Book to The List
 
 function removeBook(index) {
-   bookLibrary =bookLibrary.filter((book, bookIndex) => bookIndex !== index);
-    localStorage.setItem('bookLibrary',  JSON.stringify(bookLibrary));
+  bookLibrary = bookLibrary.filter((book, bookIndex) => bookIndex !== index);
+  localStorage.setItem('bookLibrary', JSON.stringify(bookLibrary));
 
-    createLibrary();
+  createLibrary();
 }
-
-
-
 form.addEventListener('submit', addBook);
 
 createLibrary();
+removeBook();
