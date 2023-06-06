ca3eb27a3f30d18a5ff7a4151e8365896ebc9c3e
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-06-06 06:46:55 +0300

    Added manipulations with js.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,28 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const mainContainer = document.createElement('div');
-//     mainContainer.classList.add('added-books');
-//     const paragraphOne= document.createElement('p')
-//     paragraphOne.innerHTML = "first book";
-//     const paragraphTwo = document.createElement('p');
-//     paragraphTwo.innerHTML = "Written by Robert";
-//     const underline = document.createElement('hr');
-//     const buttonRemove = document.createElement('button');
-//     buttonRemove.innerHTML='Remove';
-//     mainContainer.appendChild(paragraphOne);
-//     mainContainer.appendChild(paragraphTwo);
-//     mainContainer.appendChild(buttonRemove);
-//     mainContainer.appendChild(underline);
-//     document.getElementById('bookList').appendChild(mainContainer);
-// });
+window.addEventListener('DOMContentLoaded', () => {
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('added-books');
+    const paragraphOne= document.createElement('p')
+    paragraphOne.innerHTML = "first book";
+    const paragraphTwo = document.createElement('p');
+    paragraphTwo.innerHTML = "Written by Robert";
+    const underline = document.createElement('hr');
+    const buttonRemove = document.createElement('button');
+    buttonRemove.innerHTML='Remove';
+    mainContainer.appendChild(paragraphOne);
+    mainContainer.appendChild(paragraphTwo);
+    mainContainer.appendChild(buttonRemove);
+    mainContainer.appendChild(underline);
+    document.getElementById('bookList').appendChild(mainContainer);
 
-const title=document.querySelector('#title');
-const author=document.querySelector('#author');
-const formBtn=document.querySelector('#btn-add');
-const bookList=document.querySelector('#bookList');
- let arrayOfBooks=JSON.parse(localStorage.getItem('arrayOfBooks')) ||[];
+    function localStore(){
+        const dataStorage = JSON.parse(localStore.getItem('The Book List'));
 
+        if (localStorage.getItem("The Books List") == null) {
+            localStorage.setItem("The Books List", JSON.stringify([]));
+        }
+        
+        localStorage.setItem('The Book List', JSON.stringify(dataStorage));
+    }
 
-//function for the add
-function addBooks(){
-
-}
-
-
-
-
-//display function
-
-
-//function for remove
+    localStore();
+});
